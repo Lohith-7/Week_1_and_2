0fd90cd90fefabe6844faa77b073447e57f40c08
Lohith-7 <lohith292007@gmail.com>
2026-02-23 09:03:50 +0530

    Level 1 completed

---
+++ level1/AgeCalculator.java
@@ -0,0 +1,10 @@
+package level1;
+
+public class AgeCalculator {
+    public static void main(String[] args){
+        String name = "Harry";
+        int birthYear = 2000;
+        int age = 2024 - birthYear;
+        System.out.println(name + "'s age in 2024 is " + age);
+    }
+}
---
+++ level1/AverageMarks.java
@@ -0,0 +1,12 @@
+package level1;
+
+public class AverageMarks {
+    public static void main(String[] args){
+        String name = "Sam";
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+        float average = (maths + physics + chemistry) / 3;
+        System.out.println(name + "'s average marks in PCM is " + average); 
+    }
+}
